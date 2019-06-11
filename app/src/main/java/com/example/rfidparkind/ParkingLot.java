8cd0351d10659c39af8a703298875988e65143e4
PratapSS <pratap.solapur@gmail.com>
2019-06-11 09:08:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rfidparkind/ParkingLot.java
@@ -0,0 +1,13 @@
+package com.example.rfidparkind;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParkingLot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.parking_lot);
+    }
+}
